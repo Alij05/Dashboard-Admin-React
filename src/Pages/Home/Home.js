f155fd7ace9c1a0703ae506ab8bc09bbaee6cef5
Ali Jalilvand <ali.jalilvand2203@gmail.com>
2025-06-27 04:49:27 +0330

    Show Sale Chart to Admin with Rechart Package

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Home.css'
+import Features from '../../Components/Features/Features'
+import Chart from '../../Components/Chart/Chart'
+import { xAxisData } from '../../datas';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Features />
+      <Chart title="Month Sale" data={xAxisData} dataKey="Sale" grid />
+    </div>
   )
 }
